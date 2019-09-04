83a763d9e58f5cc64c9c295102da8056ce68a3de
Wai Lun <suenwailun@gmail.com>
2019-09-04 11:33:41 +0800

    Created Invoice.py to hold invoice class

---
+++ Invoice.py
@@ -0,0 +1,12 @@
+def convert_text_to_result (text):
+
+    result = None # TODO: Use invoice2data or other means to obtain results using text from invoice
+
+    return result
+
+def convert_text_based_pdf_to_result(invoice):
+    templates = read_templates("./templates")
+    
+    # TODO: Implement data extraction using invoice2data
+
+    return None
