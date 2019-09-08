b06622f15eeb3a3ce869e66788cd9384ab41f401
Candice Tay <candicetayjh@Hotmail.sg>
2019-09-08 22:46:14 +0800

    Added function to save output into json format

--- main.py
+++ main.py
@@ -6,11 +6,15 @@ from Invoice import Invoice
 from Token import Token
 from Classifier import Classifier
 from util import *
-
+import json
 
 print("Starting...")
 # Load invoices in specific folder
-INVOICE_PATH = "/Users/suenwailun/Sync Documents/University/Y4S1/BT3101 Business Analytics Capstone Project/Tech demo/starhub.pdf"
+#INVOICE_PATH = "/Users/candicetay/Documents/Academic/NUS/Year 4/BT3101 Capstone NatWest/PDF Invoices/Singtel Aug.pdf"
+#INVOICE_PATH = "/Users/candicetay/Documents/Academic/NUS/Year 4/BT3101 Capstone NatWest/PDF Invoices/Pest Control Service Contract.pdf"
+#INVOICE_PATH = "/Users/candicetay/Documents/Academic/NUS/Year 4/BT3101 Capstone NatWest/PDF Invoices/Dive Receipt.pdf"
+#INVOICE_PATH = "/Users/candicetay/Documents/Academic/NUS/Year 4/BT3101 Capstone NatWest/PDF Invoices/Circles April 18.pdf"
+INVOICE_PATH = "/Users/candicetay/Documents/Academic/NUS/Year 4/BT3101 Capstone NatWest/PDF Invoices/Starhub July 19.pdf"
 invoice = Invoice(INVOICE_PATH)
 page = invoice.get_page(1)
 page.do_OCR()
@@ -26,8 +30,17 @@ for i, block in page.get_tokens_by_block().items():
     print(block)
     print(" ")
 
-
 #%% Demo 3: Print page
 page.page.resize((600, 900))
 
+#%% Demo 4: Save output by extracting text from token objects for NLP experimentation
+def extractTextAttributeFromListTokens(tokenList):
+    return tokenList.text
+
+def writeOutputJson(fileName, dictData):
+    newdict = {k: list(map(extractTextAttributeFromListTokens, v)) for k, v in dictData.items()}
+    with open(fileName, 'w') as f:
+        json.dump(newdict, f, ensure_ascii=False)
+
+writeOutputJson('linebyline.txt', page.get_tokens_by_block())
 #%%
