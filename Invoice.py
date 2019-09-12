0521fa90d713386742120cbc0b1cf35d1c4007f8
ngkaiwen <ng-kai-wen@outlook.com>
2019-09-12 13:50:23 +0800

    Remove lines function, reduces | but affects structure of extracted text

--- Invoice.py
+++ Invoice.py
@@ -11,6 +11,8 @@ import nltk
 nltk.download("stopwords")
 from nltk.corpus import stopwords
 
+import cv2
+import numpy as np
 
 class Invoice:
     def __init__(self, PDF_path: str):
@@ -132,6 +134,75 @@ class InvoicePage:
             draw_rect(canvas, token, (255, 0, 0))
 
         page_copy.show()
+        
+    def remove_lines(self):
+        pil_image = self.page.convert('RGB') 
+        open_cv_image = np.array(pil_image) 
+        # Convert RGB to BGR 
+        img = open_cv_image[:, :, ::-1].copy()
+        # Convert to grey for easier processing
+        img=cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        #inverse colour og image for easier processing of lines
+        img = cv2.bitwise_not(img)
+        th2 = cv2.adaptiveThreshold(img,255, cv2.ADAPTIVE_THRESH_MEAN_C,cv2.THRESH_BINARY,15,-2)
+        #show blackwhite version
+        #cv2.imshow("blackwhite_orginal", th2)
+        #cv2.imwrite("blackwhite_orginal.jpg", th2)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+
+        horizontal = th2
+        vertical = th2
+        rows,cols = horizontal.shape
+        horizontalsize = int(cols / 15)
+        horizontalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (horizontalsize,1))
+        horizontal = cv2.erode(horizontal, horizontalStructure, (-1, -1))
+        horizontal = cv2.dilate(horizontal, horizontalStructure, (-1, -1))
+        #show horizontal lines
+        #cv2.imshow("horizontal", horizontal)
+        #cv2.imwrite("horizontal.jpg", horizontal)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        #print('horizontal')
+
+        #inverse the image, so that lines are black for masking
+        horizontal_inv = cv2.bitwise_not(horizontal)
+        #perform bitwise_and to mask the lines with provided mask
+        masked_img = cv2.bitwise_and(img, img, mask=horizontal_inv)
+        #reverse the image back to normal
+        masked_img_inv = cv2.bitwise_not(masked_img)
+        #show removal of horizontal lines
+        #cv2.imshow("masked img", masked_img_inv)
+        #cv2.imwrite("result2.jpg", masked_img_inv)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        #print('masked_img_inv')
+
+        verticalsize = int(rows / 30)
+        verticalStructure = cv2.getStructuringElement(cv2.MORPH_RECT, (1, verticalsize))
+        vertical = cv2.erode(vertical, verticalStructure, (-1, -1))
+        vertical = cv2.dilate(vertical, verticalStructure, (-1, -1))
+        #show vertical lines
+        #cv2.imshow("vertical", vertical)
+        #cv2.imwrite("vertical.jpg", vertical)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        #print('vertical')
+
+        masked_img_inv = cv2.bitwise_not(masked_img_inv)
+        #inverse the image, so that lines are black for masking
+        vertical_inv = cv2.bitwise_not(vertical)
+        #perform bitwise_and to mask the lines with provided mask
+        masked_img2 = cv2.bitwise_and(masked_img_inv, masked_img_inv, mask=vertical_inv)
+        #reverse the image back to normal
+        masked_img_inv2 = cv2.bitwise_not(masked_img2)
+        #show final result
+        #cv2.imshow("masked img2", masked_img_inv2)
+        cv2.imwrite("final_result.jpg", masked_img_inv2)
+        #cv2.waitKey(0)
+        #cv2.destroyAllWindows()
+        self.page = masked_img_inv2
+
 
 
 ##### TODO: The following code is relevant to text-based invoices and needs to be integrated
