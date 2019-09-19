aa5170ac6a7070fe40ae09c4f98de5e9971947fd
Wai Lun <suenwailun@gmail.com>
2019-09-19 22:51:12 +0800

    Removed archaic functions in util

--- util.py
+++ util.py
@@ -15,20 +15,3 @@ def convert_pdf_to_image(PDF_path):
         img = Image.frombytes(mode, [pix.width, pix.height], pix.samples)
         pages.append(img)
     return pages
-
-
-# Util function to load invoices
-def load_invoices():
-
-    # TODO: Load the invoices, store in list
-
-    return None  # Eventually return a list of invoices
-
-
-# Util function to determine if an invoice is scanned or text based
-def is_invoice_text_based(invoice):
-
-    # TODO: add code to determine if an invoice is text or image based
-
-    return True  # Eventually return result
-
