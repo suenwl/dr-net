54ae9e63397dfbbafe730eca45bb19d86b8e3bcc
ngkaiwen <ng-kai-wen@outlook.com>
2019-09-24 01:23:03 +0800

    Expanded classifier types, added svm optimisation and data scaling

--- Classifier.py
+++ Classifier.py
@@ -1,6 +1,10 @@
 from Invoice import InvoicePage, Invoice
 from Token import Token
 from sklearn import svm
+from sklearn.model_selection import GridSearchCV
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
 import pickle
 
 
@@ -18,21 +22,42 @@ class Classifier:
             text_file.write(model)
 
     def train_support_vector_machine(self, data, labels):
-        classifier = svm.SVC(gamma=0.001, C=100.0)
+        parameters = {'kernel':('linear', 'rbf'),'gamma': [0.001, 0.0001], 'C':[1, 100]}
+        #original line without use of grid search to optimise parameters
+        #classifier = svm.SVC(gamma=0.001, C=100.0)
+        svc = svm.SVC()
+        clf = GridSearchCV(svc, parameters, cv=5)
         classifier.fit(data, labels)
         self.models["Support Vector Machine"] = classifier
         self.save_model(pickle.dumps(classifier), "svm_model")
 
     def train_neural_network(self, data, labels):
-        pass
+        # multi-layer perceptron (MLP) algorithm
+        #consider increasing neuron number to match number of features as data set
+        classifier = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1)
+        classifier.fit(data, labels)
+        self.models["Neural Network"] = classifier
+        self.save_model(pickle.dumps(classifier), "nn_model")
+        
 
     def train_naive_bayes(self, data, labels):
-        pass
+        classifier = GaussianNB()
+        classifier.fit(data, labels)
+        self.models["Naive Bayes"] = classifier
+        self.save_model(pickle.dumps(classifier), "nb_model")
 
     def train_random_forest(self, data, labels):
-        pass
+        classifier = RandomForestClassifier(n_estimators=100, max_depth=2, random_state=0)
+                classifier.fit(data, labels)
+        self.models["Random Forest"] = classifier
+        self.save_model(pickle.dumps(classifier), "rf_model")
 
     def train(self, model_name: str, data, labels):
+        #mlp sensitive to feature scaling, plus NN requires this so we standardise scaling first
+        scaler = StandardScaler()
+        scaler.fit(data)  
+        data = scaler.transform(data)  
+        labels = scaler.transform(labels) 
         """ Used to train a specific model """
         if model_name == "Support Vector Machine":
             self.train_support_vector_machine(data, labels)
