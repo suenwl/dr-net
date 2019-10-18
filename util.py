76263d9c19bc1ddc62923783821e7f0b78f4f781
Wai Lun <suenwailun@gmail.com>
2019-10-18 20:29:20 +0800

    Only store images for first two and last 2 pages

--- util.py
+++ util.py
@@ -6,7 +6,11 @@ from IPython.display import clear_output, display
 def convert_pdf_to_image(PDF_path):
     document = fitz.open(PDF_path, width=1653, height=2339)
     pages = []
-    for page in document:
+    num_pages = len(document)
+    for i, page in enumerate(document):
+        if i not in [0, 1, num_pages - 2, num_pages - 1]:
+            pages.append(None)
+            continue
         zoom = 3
         mat = fitz.Matrix(
             zoom, zoom
