c7826907cc38f936c920c78081001b30e12c7bd2
Wai Lun <suenwailun@gmail.com>
2019-10-29 16:25:10 +0800

    Added train_classifier script for classifier training purposes

--- scripts/train_classifier.py
+++ scripts/train_classifier.py
@@ -1,42 +1,3 @@
-#%%
-# import methods defined in the other files
-from OCREngine import OCREngine
-from FeatureEngine import FeatureEngine
-from Invoice import Invoice
-from Token import Token
-from Classifier import Classifier
-from util import features_to_use
-
-from flask import Flask
-from flask import render_template
-from flask_socketio import SocketIO, send, emit
-
-import time
-import json
-
-app = Flask(__name__, static_folder="build/static", template_folder="build")
-app.config["SECRET_KEY"] = "mysecret"
-socketio = SocketIO(app, cors_allowed_origins="*")
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@socketio.on("req-home")
-def handle_home():
-    data = {"test": 321}
-    jsn = json.dumps(data)
-    emit("res-home", jsn)
-
-
-if __name__ == "__main__":
-    socketio.run(app, debug=True)
-
-
-"""
-
 print("Starting...")
 invoices = FeatureEngine.load_invoices_and_map_labels(
     "C:/Users/theia/Documents/Data/Year 4 Sem 1/BT3101 BUSINESS ANALYTICS CAPSTONE/Invoices",
@@ -69,4 +30,4 @@ for invoice in invoices_perf[:20]:
     )
 
 #%%
-"""
+'''
